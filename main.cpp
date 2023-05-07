ad1e7744f7a57c839611262e90af2817d39ca001
marcin <s12497@pjwstk.edu.pl>
2023-05-07 19:32:40 +0200

    Scoreboard nightmare!!!!

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <SDL.h>
 #include <iostream>
+#include <SDL_ttf.h>
+#include <string>
 
 // Screen dimensions
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 600;
 
-// Pddle dimensions
+// Paddle dimensions
 const int PADDLE_WIDTH = 20;
 const int PADDLE_HEIGHT = 100;
 
@@ -19,6 +21,10 @@ const int PADDLE_START_Y = WINDOW_HEIGHT / 2 - PADDLE_HEIGHT / 2;
 const int BALL_START_X = WINDOW_WIDTH / 2 - BALL_SIZE / 2;
 const int BALL_START_Y = WINDOW_HEIGHT / 2 - BALL_SIZE / 2;
 
+// Scoreboard initial position
+const int SCOREBOARD_X = WINDOW_WIDTH / 2;
+const int SCOREBOARD_Y = 30;
+
 // Paddle movement speed
 const int PADDLE_SPEED = 6;
 
@@ -29,12 +35,18 @@ int ballDirectionX = 1;
 int ballDirectionY = 1;
 bool ballMoving = false;
 
+// Scoreboard
+int leftScore = 0;
+int rightScore = 0;
+
 SDL_Window* window;
 SDL_Renderer* renderer;
 SDL_Event event;
 SDL_Rect rightPaddle;
 SDL_Rect leftPaddle;
 SDL_Rect ball;
+TTF_Font* font;
+SDL_Color textColor = { 255, 255, 255, 255 };
 
 bool upKeyPressed = false;
 bool downKeyPressed = false;
@@ -65,6 +77,10 @@ void initialize()
     ball.y = BALL_START_Y;
     ball.w = BALL_SIZE;
     ball.h = BALL_SIZE;
+
+    // Scoreboard
+    TTF_Init();
+    font = TTF_OpenFont("arial.ttf", 40);
 }
 
 void handleInput()
@@ -98,6 +114,7 @@ void handleInput()
             }
         }
     }
+
 }
 
 void movePaddle()
@@ -112,6 +129,23 @@ void movePaddle()
         leftPaddle.y += 1;  // Increase the y-position change for slower movement
 }
 
+
+void resetBall()
+{
+    ball.x = BALL_START_X;
+    ball.y = BALL_START_Y;
+    ballSpeed = INIT_BALL_SPEED;
+    ballMoving = false;
+}
+
+void increaseLeftScore()
+{
+    leftScore++;
+}
+void increaseRightScore()
+{
+    rightScore++;
+}
 void moveBall()
 {
     if (ballMoving)
@@ -130,13 +164,16 @@ void moveBall()
             ballSpeed += 0.2f;
         }
 
-        if (ball.x <= 0 || ball.x >= WINDOW_WIDTH - BALL_SIZE)
+        if (ball.x <= 0)
         {
-            ball.x = BALL_START_X;
-            ball.y = BALL_START_Y;
-            ballSpeed = INIT_BALL_SPEED;
-            ballMoving = false;
+            increaseRightScore();
+            resetBall();
+        }
 
+        if (ball.x >= WINDOW_WIDTH - BALL_SIZE)
+        {
+            increaseLeftScore();
+            resetBall();
         }
     }
 
@@ -150,6 +187,29 @@ void render()
     SDL_RenderFillRect(renderer, &rightPaddle);
     SDL_RenderFillRect(renderer, &leftPaddle);
     SDL_RenderFillRect(renderer, &ball);
+
+    // Scoreboard
+    // Render the scores
+    std::string leftScoreStr = std::to_string(leftScore);
+    std::string rightScoreStr = std::to_string(rightScore);
+
+    SDL_Surface* leftScoreSurface = TTF_RenderText_Solid(font, leftScoreStr.c_str(), textColor);
+    SDL_Texture* leftScoreTexture = SDL_CreateTextureFromSurface(renderer, leftScoreSurface);
+
+    SDL_Surface* rightScoreSurface = TTF_RenderText_Solid(font, rightScoreStr.c_str(), textColor);
+    SDL_Texture* rightScoreTexture = SDL_CreateTextureFromSurface(renderer, rightScoreSurface);
+
+    SDL_Rect leftScoreRect = { WINDOW_WIDTH / 4, 10, leftScoreSurface->w, leftScoreSurface->h };
+    SDL_Rect rightScoreRect = { WINDOW_WIDTH * 3 / 4, 10, rightScoreSurface->w, rightScoreSurface->h };
+
+    SDL_RenderCopy(renderer, leftScoreTexture, NULL, &leftScoreRect);
+    SDL_RenderCopy(renderer, rightScoreTexture, NULL, &rightScoreRect);
+
+    SDL_DestroyTexture(leftScoreTexture);
+    SDL_FreeSurface(leftScoreSurface);
+    SDL_DestroyTexture(rightScoreTexture);
+    SDL_FreeSurface(rightScoreSurface);
+
     SDL_RenderPresent(renderer);
 }
 
@@ -162,7 +222,16 @@ void cleanup()
 
 int main(int argc, char* argv[])
 {
+    SDL_SetError(SDL_GetError());  // Clear any existing error
+    SDL_ClearError();  // Clear the SDL error state
+
     initialize();
+
+    // Print SDL error, if any
+    const char* sdlError = SDL_GetError();
+    if (sdlError[0] != '\0') {
+        std::cout << "SDL Error: " << sdlError << std::endl;
+    }
 
     while (true)
     {
